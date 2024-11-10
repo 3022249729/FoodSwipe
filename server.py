dd6c86b01b131bd928a00ad388deeba0b667e0ed
Ren Chen <xuanrenchen7@gmail.com>
2024-11-09 21:40:21 -0500

    Cached restaurant info instead of new api request every landing.

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import random
 import string
 import requests
 from time import sleep
+import json
 
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
@@ -69,16 +70,22 @@ def callback():
 
 @app.route('/create_session', methods=['POST'])
 def create_session():
-    data = request.get_json()
-    
-    latitude = data.get('latitude')
-    longitude = data.get('longitude')
+    # data = request.get_json() 
+    # latitude = data.get('latitude')
+    # longitude = data.get('longitude')
+    # restaurants = get_restaurants(latitude, longitude)
 
-    restaurants = get_restaurants(latitude, longitude)
+    # for dynamic fetches that uses google maps api every call
+    # we are sticking to cached data because it takes a long time to make api calls
+
+    with open('restaurants_info.txt', 'r') as f:
+        raw_data = f.read()
+    restaurants = json.loads(raw_data)
+
     return jsonify(restaurants), 200
 
 
-def get_restaurants(latitude, longitude, radius=5000):
+def get_restaurants(latitude, longitude, radius=8000):
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     params = {
         'location': f"{latitude},{longitude}",
@@ -113,11 +120,10 @@ def get_restaurants(latitude, longitude, radius=5000):
                 'rating': rating,
                 'rating_amount': rating_amount,
                 'address': address,
-                'photo_url': photo_url 
+                'photo_url': photo_url,
+                'maps_url': f"https://www.google.com/maps/place/?q=place_id:${id}"
             })
 
-        if len(restaurants) >= 50:
-            break
 
         token = data.get('next_page_token')
         if token:
@@ -126,6 +132,8 @@ def get_restaurants(latitude, longitude, radius=5000):
         else:
             break
         
+    with open('restaurants_info.txt', 'w') as file:
+        json.dump(restaurants, file, indent=4)
     return restaurants
 
     
