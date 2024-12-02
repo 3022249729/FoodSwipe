3b67a430f452e774a13c8f5584b359295a2c9a30
Ren Chen <xuanrenchen7@gmail.com>
2024-12-02 00:09:10 -0500

    Added https redirect

--- server.py
+++ server.py
@@ -213,6 +213,12 @@ def create_session():
 
     return jsonify({"restaurants": restaurants}), 200
 
+@app.before_request
+def enforce_https():
+    if not request.is_secure and not app.debug:
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
+
 def load_restaurants():
     """Check if 'restaurants_info.txt' exists; if not, generate and save restaurant data."""
     if not os.path.exists('restaurants_info.txt'):
@@ -297,4 +303,4 @@ def get_restaurants(latitude, longitude, radius=8000):
     return restaurants
     
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0" ,port=3000, debug=True)
+    socketio.run(app, host="0.0.0.0" ,port=3000)
